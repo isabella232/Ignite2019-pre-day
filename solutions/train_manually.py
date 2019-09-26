a0dd8c61d77e402bbc6b242897854004c6f768c6
Seth Mottaghinejad <sethmott@microsoft.com>
2019-09-25 22:04:21 -0700

    One successful run

--- solutions/train_manually.py
+++ solutions/train_manually.py
@@ -2,7 +2,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from azureml.train.automl.model_wrappers import LightGBMClassifier
 
-manual_model = Pipeline(memory=None, steps=[('StandardScaler', StandardScaler(**scaler_params)), ('LightGBMClassifier', LightGBMClassifier(**model_params))])
+step0 = fitted_model.get_params(0)['steps'][0][1]
+step1 = fitted_model.get_params(0)['steps'][1][1]
+manual_model = Pipeline(memory=None, steps=[('StandardScaler', step0), ('LightGBMClassifier', step1)])
 
 manual_model.fit(X_train.values, y_train.values[:, 0])
 y_pred = manual_model.predict(X_test)
